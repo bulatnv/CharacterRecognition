3aef571913932f6f1cc644cff574b55d0d75252c
bulatnv88 <bulatnv@gmail.com>
2019-10-23 00:14:50 +0500

    Working on Bulat's handwritting dataset

--- cnn.py
+++ cnn.py
@@ -12,9 +12,9 @@ def crop_frame(frame, height, width, density):
 
 def split_image(frame):
     cs = []
-    rows = np.vsplit(frame, 34)
+    rows = np.vsplit(frame, 33)
     for row in rows:
-        row_cs = np.hsplit(row, 24)
+        row_cs = np.hsplit(row, 25)
         for c in row_cs:
             # for kNN
             # c = c.flatten()
@@ -24,14 +24,14 @@ def split_image(frame):
 
 def process_frame(frame):
     digits = cv2.imread(frame, cv2.IMREAD_GRAYSCALE)
-    digits = crop_frame(digits, 34, 24, 50)
+    digits = crop_frame(digits, 33, 25, 50)
     return np.array(split_image(digits), dtype=np.float32)
 
 
 def prepare_test_data(frame):
     test = cv2.imread(frame, cv2.IMREAD_GRAYSCALE)
-    test = crop_frame(test, 34, 1, 50)
-    test_letters = np.vsplit(test, 34)
+    test = crop_frame(test, 33, 1, 50)
+    test_letters = np.vsplit(test, 33)
     test_cells = []
     for d in test_letters:
         # d = d.flatten()
@@ -39,12 +39,13 @@ def prepare_test_data(frame):
     return np.array(test_cells, dtype=np.float32)
 
 
+# processing images
 cells = []
-cells1 = process_frame("letters_1.BMP")
-cells2 = process_frame("letters_2.BMP")
-cells3 = process_frame("letters_3.BMP")
-cells4 = process_frame("letters_4.BMP")
-cells5 = process_frame("letters_5.BMP")
+cells1 = process_frame("201.png")
+cells2 = process_frame("201.png")
+cells3 = process_frame("201.png")
+cells4 = process_frame("201.png")
+cells5 = process_frame("201.png")
 cells = np.concatenate((cells1, cells2, cells3, cells4, cells5), axis=0)
 cells = cells[:, :, :, None]
 # print(cells.shape)
@@ -53,15 +54,17 @@ cells = cells[:, :, :, None]
 # cv2.waitKey()
 # print(len(cells))
 
+# Creating labels
 k_labels = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ', 'Э', 'Ю', 'Я']
 k = range(33)
 cells_labels = np.repeat(k, 25)
 labels = np.concatenate((cells_labels, cells_labels, cells_labels, cells_labels, cells_labels), axis=None)
-test_cells_labels = range(33)
+
 
 # Test data preparation
-test_cells = prepare_test_data("test_letters.png")  # test_letters2.BMP
+test_cells = prepare_test_data("test.png")  # test_letters2.BMP
 test_cells = test_cells[:, :, :, None]
+test_cells_labels = np.arange(33)
 # print(test_cells.shape)
 
 ## Normalization
@@ -83,7 +86,7 @@ model1.summary()
 model1.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-model1.fit(cells, cells_labels, shuffle=True, epochs=20)
+model1.fit(cells, labels, shuffle=True, epochs=3)
 
 test_loss, test_acc = model1.evaluate(test_cells, test_cells_labels)
 
